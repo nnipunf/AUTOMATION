8f05a7015acd7e341ea5aa63da4bbe0f40a9aac9
nipunBN <nipunferdous406@gmail.com>
2022-09-15 17:30:21 +0600

    scrapping from website

--- Test_Practice/amazon_scrapping/script_01.py
+++ Test_Practice/amazon_scrapping/script_01.py
@@ -3,6 +3,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from openpyxl import Workbook
+import smtplib
+from email.message import EmailMessage  
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
@@ -14,11 +17,43 @@ driver.find_element(By.XPATH,"//span[text()='SAMSUNG']").click()
 phonenames = driver.find_elements(By.XPATH, "//span[contains(@class, 'a-size-medium a-color-base a-text-normal')]")
 prices = driver.find_elements(By.XPATH, "//span[contains(@class, 'price-whole')]")
 
+myphone = []
+myprice = []
+
 for phone in phonenames:
-    print(phone.text)
+    #print(phone.text)
+    myphone.append(phone.text)
+
 print("*"*50)
 
 for price in prices:
-    print(price.text)
+    #print(price.text)
+    myprice.append(price.text)
 
-driver.quit()
+finalList = zip(myphone, myprice)
+#for data in list(finalList):
+    #print(data)
+
+print("PART01")
+###################################################
+
+
+###store data in a excel file with openpyXL
+
+##object of workbook class
+wb = Workbook()
+wb['Sheet'].title = 'Samsung Data'
+#sheet01 of work book
+sheet1 = wb.active
+sheet1.append(['Name', 'Price'])
+
+
+for x in list(finalList):
+    sheet1.append(x)
+
+wb.save("FinalRecords.xlsx")
+
+print("PART02")
+###################################################
+
+#driver.quit()
